26bded80acec2687a72a6dc5b978083b713410e7
emege93 <emege93@gmail.com>
2020-10-03 13:08:26 +0200

    Get One - update

--- router/pets.js
+++ router/pets.js
@@ -31,7 +31,81 @@ router.post('/', async(req,res) => {
 
     res.redirect('/mascotas');
   } catch (error) {
-    
+
+  }
+})
+
+router.get('/:id', async (req, res) => {
+
+  const id = req.params.id
+
+  try {
+
+    const mascotaDB = await Mascota.findOne({_id: id})
+
+    res.render('detalle', {
+      mascota: mascotaDB,
+      error:false
+    })
+
+  } catch (error) {
+
+    res.render('detalle', {
+      error:true,
+      mensaje: 'No se encuentra el ID'
+    })
+
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+
+  const id = req.params.id
+
+  try {
+    const mascotaDB = await Mascota.findByIdAndDelete({_id: id})
+
+    if(mascotaDB) {
+
+      res.json({
+        estado: true,
+        mensaje: 'Eliminado Correctamente'
+      });
+
+    } else {
+
+      res.json({
+        estado: false,
+        mensaje: 'fallo al eliminar'
+      });
+
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  const id = req.params.id
+  const body = req.body
+
+  try {
+
+    const mascotaDB = await Mascota.findByIdAndUpdate(id, body, { useFindAndModify: false })
+    console.log(mascotaDB);
+
+    res.json({
+      estado:true,
+      mensaje: 'Editado'
+    })
+
+  } catch (error) {
+    console.log(error);
+
+    res.json({
+      estado:false,
+      mensaje: 'Fallado'
+    })
   }
 })
 
